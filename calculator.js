e0fa5a52b3dfc11be4b01fa98f9ba49eca947a7f
Karun Jaraslertsuwan <oxygenyoyo@Karuns-MacBook-Air.local>
2016-03-12 13:29:17 +0700

    First commit

---
+++ calculator.js
@@ -0,0 +1,25 @@
+function add(num1, num2) {
+	return num1 + num2
+}
+
+function minus (num1, num2) {
+	return num1 - num2
+}
+
+function devide (num1, num2) {
+	return num1 / num2
+}
+
+function multiple (num1, num2) {
+	return num1 * num2
+}
+
+function sum (num1, num2, num3) {
+	return num1 + num2 + num3
+}
+
+module.exports.add = add
+module.exports.minus = minus
+module.exports.devide = devide
+module.exports.multiple = multiple
+module.exports.sum = sum
---
+++ main.js
@@ -0,0 +1,7 @@
+
+var calculator = require('ywcworkshop-benz');
+
+console.log(calculator.add(10,20));
+console.log(calculator.minus(10,20));
+console.log(calculator.div(10,20));
+console.log(calculator.multiply(10,20));
